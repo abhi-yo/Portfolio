31290a7541bc112794b5c6a59d1005fcb8ea634f
abhi-yo <akshatsing11@gmail.com>
2023-12-04 08:57:34 +0530

    :sparkles: add images

--- src/data/data.js
+++ src/data/data.js
@@ -1,33 +1,27 @@
-import WorkIm from "../assets/projects/workImg.jpeg";
-import RealEst from "../assets/projects/realestate.jpg";
+import Razorpay from "../assets/projects/razorpay.png";
+import Ecomzy from "../assets/projects/ecomzy.png";
+import RandomGif from "../assets/projects/randomgif.png";
 
 export const data = [
   {
     id: 1,
-    name: "React JS Application",
-    image: WorkIm,
-    github: "",
-    live: "",
+    name: "RazorPay Clone Application",
+    image: Razorpay,
+    github: "https://github.com/abhi-yo/razorpay-clone",
+    live: "https://razorpay-clone-done.netlify.app/",
   },
   {
     id: 2,
-    name: "React JS Application",
-    image: RealEst,
-    github: "",
-    live: "",
+    name: "ECOMZY Shopping Cart",
+    image: Ecomzy,
+    github: "https://github.com/abhi-yo/shoppingCart-webapp",
+    live: "https://shopping-cart-webapp.vercel.app/",
   },
   {
     id: 3,
-    name: "React JS Application",
-    image: WorkIm,
-    github: "",
-    live: "",
-  },
-  {
-    id: 4,
-    name: "React JS Application",
-    image: RealEst,
-    github: "",
-    live: "",
+    name: "Gif Generator Application",
+    image: RandomGif,
+    github: "https://github.com/abhi-yo/gif-generator",
+    live: "https://gif-generator-two.vercel.app/",
   },
 ];
