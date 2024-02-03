3d87d46a4319699496623254ad565ac77c0dbe08
ok-akshat <akshatm.com@gmail.com>
2024-02-04 00:04:09 +0530

    refactor: update link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -106,7 +106,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1g1aBSRX2sVZRJZXcauJeeTgpxCk5VPc6/view"
+              href="https://drive.google.com/file/d/1vzM8pcPrYRN4M2YMi1LQBTU-h1v3YsBH/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
